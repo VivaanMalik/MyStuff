0c6c4e9eda1abb7fab131e511cd92dbc7e5df60d
Vivaan Da Gr8 <malikvivaan@gmail.com>
2021-12-11 16:42:47 +0530

    prime num

---
+++ CryptoBoii/encode/RSA - remake/main.py
@@ -0,0 +1,12 @@
+def JenKeyz(): #GenKeys
+    pass
+
+class Publikee(): # Public Key
+    def __init__(self, sections:int, value:list):
+        self.value=value
+        self.sect=sections
+
+class Praivetkee: # Private Key
+    def __init__(self, sections:int, value:list):
+        self.value=value
+        self.sect=sections
